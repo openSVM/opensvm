d47bfc72740d904169a8267bcf15cb44e0c13480
0xrinegade <0xrinegade@gmail.com>
2024-12-31 20:02:49 +0300

    Add aggressive type checking disabling in tsconfig and next.config

--- next.config.js
+++ next.config.js
@@ -17,6 +17,18 @@ const nextConfig = {
     }
     return config;
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
